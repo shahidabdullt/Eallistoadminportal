a121aad0ff343f46a9bb53a668a798a6da0debd9
shahidabdullt <shahidabdul200@gmail.com>
2025-04-04 23:34:51 +0530

    session

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
         ]);
        
         if (Auth::attempt($credentials)) {
-            // $request->session()->regenerate(); 
+             $request->session()->regenerate(); 
             $user = Auth::user();
             
             if ($user->isadmin=="1") {
